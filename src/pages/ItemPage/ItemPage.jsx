b8d85dbf0311c1b11fa9a6c8e32b9e37952463af
Takaya Hirose <ballybumps0601@gmail.com>
2021-01-04 21:44:00 -0800

    MPA-44: Remove unnecessary files,

--- src/pages/ItemPage/ItemPage.jsx
+++ src/pages/ItemPage/ItemPage.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Input } from 'reactstrap';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumbs';
 import star from '../../assets/star.svg'
