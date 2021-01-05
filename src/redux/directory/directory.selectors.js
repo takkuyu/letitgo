043c8da07cf36a18033aa130da4a0927be3363fc
Takaya Hirose <ballybumps0601@gmail.com>
2021-01-04 21:44:00 -0800

    MPA-44: Remove unnecessary files,

--- src/redux/directory/directory.selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectDirectory = (state) => state.directory;
-
-export const selectDirectoryCategories = createSelector(
-  [selectDirectory],
-  (directory) => directory.categories
-);
